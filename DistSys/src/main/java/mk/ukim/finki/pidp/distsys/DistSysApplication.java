55dc6dd7f7352a35526a54e524f3eedce0050414
Nasteskii <borce.nasteski123@gmail.com>
2022-12-16 23:54:21 +0100

    Initial commit

---
+++ DistSys/src/main/java/mk/ukim/finki/pidp/distsys/DistSysApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.pidp.distsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistSysApplication.class, args);
+	}
+
+}
