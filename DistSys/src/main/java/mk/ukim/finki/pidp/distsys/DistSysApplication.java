02c8caf36de99fef3e770e63637726f8a164638c
Nasteskii <borce.nasteski123@gmail.com>
2022-12-17 01:23:22 +0100

    Modified site header

--- DistSys/src/main/java/mk/ukim/finki/pidp/distsys/DistSysApplication.java
+++ DistSys/src/main/java/mk/ukim/finki/pidp/distsys/DistSysApplication.java
@@ -3,6 +3,9 @@ package mk.ukim.finki.pidp.distsys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication()
 public class DistSysApplication {
@@ -11,4 +14,8 @@ public class DistSysApplication {
         SpringApplication.run(DistSysApplication.class, args);
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
 }
